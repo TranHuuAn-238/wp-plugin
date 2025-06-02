9d73ab6908a8bae8e6c7b6cda84e932e84645e79
TranHuuAn-238 <tranhuuan2308@gmail.com>
2025-06-02 22:11:52 +0700

    cau truc thu muc plugin

---
+++ wp-ecommerce.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Plugin Name:       WP - Ecommerce
+ * Plugin URI:        #
+ * Description:       Plugin for test.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Siêu Rơm
+ * Author URI:        #
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        #
+ * Text Domain:       wp-ecommerce
+ * Domain Path:       /languages
+ */
+
+// Định nghĩa các hằng số của plugin
+define('WP_PATH', plugin_dir_path(__FILE__)); // include các file php khác
+define('WP_URI', plugin_dir_url(__FILE__));   // include js, css, images
+
+// Định nghĩa hành động khi plugin được kích hoạt
+register_activation_hook( __FILE__, 'wp_ecommerce_activation' );
+function wp_ecommerce_activation() {
+    // Không được có output (echo ở đây)
+    // Tạo CSDL
+
+    // Tạo dữ liệu mẫu
+
+    // Tạo options
+}
+
+// Định nghĩa hành động khi plugin ngừng kích hoạt
+register_deactivation_hook( __FILE__, 'wp_ecommerce_deactivation' );
+function wp_ecommerce_deactivation() {
+    // Có thể xóa CSDL
+
+    // Xóa options
+}
