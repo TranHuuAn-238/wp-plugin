db891d5a711491515feb2822da6831686eee3f2a
TranHuuAn-238 <tranhuuan2308@gmail.com>
2025-06-04 21:52:14 +0700

    tao taxonomy moi

--- includes/post_types.php
+++ includes/post_types.php
@@ -19,3 +19,32 @@ function wp_custom_post_type() {
 		)
 	);
 }
+
+
+add_action( 'init', 'wp_register_taxonomy_product' );
+function wp_register_taxonomy_product() {
+    $labels = array(
+        'name'              => _x( 'Danh mục', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Danh mục', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Danh mục' ),
+        'all_items'         => __( 'All Danh mục' ),
+        'parent_item'       => __( 'Parent Danh mục' ),
+        'parent_item_colon' => __( 'Parent Danh mục:' ),
+        'edit_item'         => __( 'Edit Danh mục' ),
+        'update_item'       => __( 'Update Danh mục' ),
+        'add_new_item'      => __( 'Add New Danh mục' ),
+        'new_item_name'     => __( 'New Danh mục' ),
+        'menu_name'         => __( 'Danh mục' ),
+    );
+    $args   = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => [ 'slug' => 'course' ], // danh-muc
+    );
+	register_taxonomy( 'product_cat', [ 'product' ], $args );
+
+    // http://phongvu.vn/danh-muc/iphone
+}
